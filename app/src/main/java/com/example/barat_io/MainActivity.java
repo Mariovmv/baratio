7ceb026bd1ae935f8c8270027ba25bb90c516056
ELtrunco <mario00375@gmail.com>
2022-06-02 01:37:22 -0500

    boton salir

---
+++ app/src/main/java/com/example/barat_io/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.barat_io;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button salir, iniciar;
+    EditText user, pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        salir = findViewById(R.id.salir);
+        iniciar = findViewById(R.id.iniciar);
+        user = findViewById(R.id.user);
+        pass = findViewById(R.id.pass);
+
+
+
+
+salir.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(MainActivity.this, "Se cerro la aplicacion", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+});
+
+
+iniciar.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+
+    }
+});
+
+    }
+}
